e720e02e77d93ec83ac372d592fa3c608e383723
xiaolei1998 <xiaolei6@ualberta.ca>
2022-06-17 12:27:19 +0800

    Helloworld example C project
    test pass

---
+++ Helloworld/Debug/application/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../application/main.c 
+
+OBJS += \
+./application/main.o 
+
+C_DEPS += \
+./application/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+application/%.o: ../application/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU RISC-V Cross C Compiler'
+	riscv-nuclei-elf-gcc -march=rv32imac -mabi=ilp32 -mcmodel=medany -mno-save-restore -O2 -ffunction-sections -fdata-sections -fno-common  -g -D__IDE_RV_CORE=e203 -DSOC_HBIRD -DDOWNLOAD_MODE=DOWNLOAD_MODE_ILM -DDOWNLOAD_MODE_STRING=\"ILM\" -DBOARD_HBIRD_EVAL -I"C:\Users\Michelle ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include" -I"C:\Users\Michelle ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\SoC\hbird\Common\Include" -I"C:\Users\Michelle ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\SoC\hbird\Board\hbird_eval\Include" -I"C:\Users\Michelle ZHANG\Documents\Nuclei\workspace\Helloworld\application" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Helloworld/Debug/hbird_sdk/SoC/hbird/Common/Source/Drivers/hbird_uart.d
@@ -0,0 +1,30 @@
+hbird_sdk/SoC/hbird/Common/Source/Drivers/hbird_uart.o: \
+ ../hbird_sdk/SoC/hbird/Common/Source/Drivers/hbird_uart.c \
+ C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\SoC\hbird\Common\Include/hbird.h \
+ C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/nmsis_core.h \
+ C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/nmsis_version.h \
+ C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/nmsis_compiler.h \
+ C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/nmsis_gcc.h \
+ C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/riscv_encoding.h \
+ C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/riscv_bits.h \
+ C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/core_feature_base.h \
+ C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/core_feature_plic.h \
+ C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/core_feature_timer.h \
+ C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/core_feature_fpu.h \
+ C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/core_compatiable.h \
+ C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\SoC\hbird\Common\Include/system_hbird.h \
+ C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\SoC\hbird\Common\Include/hbird_uart.h
+C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\SoC\hbird\Common\Include/hbird.h:
+C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/nmsis_core.h:
+C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/nmsis_version.h:
+C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/nmsis_compiler.h:
+C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/nmsis_gcc.h:
+C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/riscv_encoding.h:
+C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/riscv_bits.h:
+C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/core_feature_base.h:
+C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/core_feature_plic.h:
+C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/core_feature_timer.h:
+C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/core_feature_fpu.h:
+C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\NMSIS\Core\Include/core_compatiable.h:
+C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\SoC\hbird\Common\Include/system_hbird.h:
+C\:\Users\Michelle\ ZHANG\Documents\Nuclei\workspace\Helloworld\hbird_sdk\SoC\hbird\Common\Include/hbird_uart.h:
